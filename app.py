0af84b91acc13142237dc2b5a633f2d7086990f7
juanda <jdrincone@gmail.com>
2025-05-11 10:04:05 -0500

    Actualización y ajustes finales para despliegue en la nube

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from dotenv import load_dotenv
 
 # Configuración de la base de datos
 load_dotenv()
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL="postgresql://postgres:R1NC0N0329*@db.qujddvkvvueukqvuqoai.supabase.co:5432/postgres"
+#DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
